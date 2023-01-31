03b46e50c2a77154dbee733c2fa770e690500be5
Тимур <lorenz64@mail.ru>
2023-01-31 16:22:42 +0300

    работает составление списков

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import zipfile as zf_module
+from pprint import pprint
+
 
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
@@ -19,9 +22,39 @@
 # +---------+----------+
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
-# Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+# Требования к коду: он должен быть готовым к расширению функциональности.
+# Делать сразу на классах.
 
-# TODO здесь ваш код
+# cp1251
+class Lists:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.stats = {}
+        self.count_list = []
+        self.char_list = []
+
+    def make_lists(self):
+        with open(self.filename, mode='r', encoding='cp1251') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in self.stats:
+                            self.stats[char] += 1
+                        else:
+                            self.stats[char] = 1
+                    else:
+                        pass
+
+        for char, count in self.stats.items():
+            self.count_list.append(count)
+            self.char_list.append(char)
+
+
+voyna_i_mir = Lists('voyna-i-mir.txt')
+voyna_i_mir.make_lists()
+pprint(voyna_i_mir.count_list)
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
