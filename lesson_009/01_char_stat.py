1c21955af70ef70acee9b71de9c8200b7219a2bc
Тимур <lorenz64@mail.ru>
2023-02-02 19:03:54 +0300

    оба типа сортировки работают, нужна запаковка в классы

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -31,9 +31,10 @@ class Lists:
     def __init__(self, filename, encoding):
         self.filename = filename
         self.encoding = encoding
-        self.stats = {}
-        self.count_list = []
-        self.char_list = []
+        self.stats_chars_qnt = {}
+        self.stats_qnt_chars = {}
+        # self.count_list = []
+        # self.char_list = []
         self.char_total = 0
 
     def make_lists(self):
@@ -41,17 +42,18 @@ class Lists:
             for line in file:
                 for char in line:
                     if char.isalpha():
-                        if char in self.stats:
-                            self.stats[char] += 1
+                        if char.lower() in self.stats_chars_qnt:
+                            self.stats_chars_qnt[char.lower()] += 1
                         else:
-                            self.stats[char] = 1
+                            self.stats_chars_qnt[char.lower()] = 1
                         self.char_total += 1
-                    else:
-                        pass
 
-        for char, count in self.stats.items():
-            self.count_list.append(count)
-            self.char_list.append(char)
+        for k, v in self.stats_chars_qnt.items():#отзеркалил словарь
+            self.stats_qnt_chars[v] = k
+
+        # for char, count in self.stats_chars_qnt.items():#не оч нужно, удалить
+        #     self.count_list.append(count)
+        #     self.char_list.append(char)
 
 
 # форматирование
@@ -60,27 +62,32 @@ class Lists:
 count_char = Lists(filename='voyna-i-mir.txt', encoding='cp1251')
 count_char.make_lists()
 
+# верхний блок таблицы
 column_width = 15
-
 print(f'+{"+" :-^{column_width * 2 + 2}}+')
 print(f'|{"Буква":^{column_width}}|',
       f'{"Частота":^{column_width}}|')
 print(f'+{"+" :-^{column_width * 2 + 2}}+')
 
-char_ascend = sorted(count_char.stats.keys())
-char_decsend = sorted(count_char.stats.keys(), reverse=True)
-count_ascend = sorted(count_char.stats.values())
-count_decsend = sorted(count_char.stats.values(), reverse=True)
+char_ascend = sorted(count_char.stats_chars_qnt.keys())
+char_descend = sorted(count_char.stats_chars_qnt.keys(), reverse=True)
+num_ascend = sorted(count_char.stats_qnt_chars.keys())
+num_descend = sorted(count_char.stats_qnt_chars.keys(), reverse=True)
 
-for char in char_ascend:
-    print(f'|{char:^{column_width}}|',
-          f'{count_char.stats.get(char):^{column_width}}|')
+# for char in char_descend:  # работает сортировка по символам в обе стороны
+#     print(f'|{char:^{column_width}}|',
+#           f'{count_char.stats_chars_qnt.get(char):^{column_width}}|')
 
+for qnt in num_descend:  # работает сортировка по количеству в обе стороны
+    print(f'|{count_char.stats_qnt_chars.get(qnt):^{column_width}}|',
+          f'{qnt:^{column_width}}|')
 
+# нижний блок таблицы
 print(f'+{"+" :-^{column_width * 2 + 2}}+')
 print(f'|{"Итого":^{column_width}}|',
       f'{count_char.char_total:^{column_width}}|')
 print(f'+{"+" :-^{column_width * 2 + 2}}+')
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
