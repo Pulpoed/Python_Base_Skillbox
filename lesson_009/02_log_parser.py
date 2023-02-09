8d7e4e10fd27b30163b7eb4689192905015f5a78
Тимур <lorenz64@mail.ru>
2023-02-09 13:30:35 +0300

    криво косо но выполнено задание

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from pprint import pprint
+
+
 # Имеется файл events.txt вида:
 #
 # [2018-05-17 01:55:52.665804] NOK
@@ -20,22 +22,50 @@ from pprint import pprint
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-#_________
+# _________
+class Logger:
 
-parser = []
-log = open('events.txt', mode='r')
-for line in log:
-    if 'NOK' in line[29:32]:
-        line_cut = line[1:17]
-        datetime_object = datetime.strptime(line_cut, '%Y-%m-%d %H:%M')
-        parser.append(datetime_object)
-log.close()
+    def __init__(self, filename):
+        self.parsed = []
+        self.filename = filename
 
-# if datetime_object in parser:
-#     parser[datetime_object] += 1
-# else:
-#     parser[datetime_object] = 1
-#___________
+    def parse(self):
+        log = open(file=self.filename, mode='r')
+        for line in log:
+            if 'NOK' in line[29:32]:
+                line_cut = line[1:17]
+                datetime_object = datetime.strptime(line_cut, '%Y-%m-%d %H:%M')
+                self.parsed.append(datetime_object)
+        log.close()
+
+
+events = Logger('events.txt')
+events.parse()
+
+print(type(events.parsed[0]))
+
+events_sorted = {}
+
+for event in events.parsed:
+    event_cut = event.year, event.month, event.day, event.hour, event.minute
+    if event_cut in events_sorted:
+        events_sorted[event_cut] += 1
+    else:
+        events_sorted[event_cut] = 1
+
+print(events_sorted.keys())
+
+file_out = 'file_out.txt'
+file = open(file_out, mode='w')
+for date, qnt in events_sorted.items():
+    file_content = str(datetime(date[0],
+                                date[1],
+                                date[2],
+                                date[3],
+                                date[4]).isoformat(sep=' ', timespec='minutes')) + ' : ' + str(qnt) + '\n'
+    file.write(file_content)
+file.close()
+# ___________
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
