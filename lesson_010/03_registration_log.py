1eb538d4c714977c75af459880bb767793f859d6
Тимур <lorenz64@mail.ru>
2023-02-13 17:04:32 +0300

    задание 3 выполнено

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,49 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+def reg(line):
+    """Пытается вписать в три переменные имя, почту и возраст"""
+    name, mail, age = line.split(' ')
+    age = int(age)
+    for char in name:
+        if not char.isalpha():
+            raise NotNameError('Некорректное имя')
+    if not '@' in line or not '.' in line:
+        raise NotEmailError('Некорректная почта')
+    if not 10 <= age <= 99:
+        raise ValueError('Некорректный возраст')
+    return name, mail, age
+
+
+file_raw = 'registrations.txt'
+file_out = 'end_clear.txt'
+file_errors = 'end_errors.txt'
+new_file = open(file_out, mode='w')
+new_file_errors = open(file_errors, mode='w')
+with open(file_raw, mode='r', encoding='utf8') as file:
+    for line in file:
+        try:
+            new_line = f'{reg(line)[0]} {reg(line)[1]} {reg(line)[2]}\n'
+            new_file.write(new_line)
+        except ValueError as exc:
+            if 'unpack' in exc.args[0]:
+                new_file_errors.write(f'Недостаточно полей {exc} в строке {line}')
+            elif 'Некорректный' in exc.args[0]:
+                new_file_errors.write(f'{exc} в строке {line}')
+            else:
+                new_file_errors.write(f'{exc} в строке {line}')
+        except NotNameError as exc:
+            new_file_errors.write(f'{exc} в строке {line}')
+        except NotEmailError as exc:
+            new_file_errors.write(f'{exc} в строке {line}')
+
+new_file.close()
+new_file_errors.close()
